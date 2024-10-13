8ff437ac8e158ed9a6867b51dc736b5c9d18901b
sakshi <sakshixalxo@gmail.com>
2024-10-14 02:28:44 +0530

    NewsLetter and Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
 
         
        </Routes>
+       <Footer/>
       </BrowserRouter>
       
     </div>
--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -3,8 +3,13 @@ import './NewsLetter.css'
 
 const NewsLetter = () => {
   return (
-    <div>
-      
+    <div className='newsLetter'>
+      <h1>Get Exclusive Offers On your Email</h1>
+      <p>Subscribe to our newsLetter and stay updated</p>
+      <div>
+        <input type="email" placeholder='Enter your E-mail id' />
+        <button>Subscribe</button>
+      </div>
     </div>
   )
 }
